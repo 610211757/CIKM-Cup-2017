7db37ee1091c98da06a5ab169b4ad05000113a49
610211757 <610211757@qq.com>
2017-05-18 10:18:32 +0800

    final class=4

--- gbdt_pre_classifier.py
+++ gbdt_pre_classifier.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import GradientBoostingClassifier
 import os
 from sklearn.externals import joblib
 
+
 def getDataSet1(n):
     testName_all = []
     rainfall_all = []
@@ -13,9 +14,9 @@ def getDataSet1(n):
     dataSet_label = []
     i = 0
     k = 1000 * n
-    f = open('E:\\李卓聪\\rain_predict\\data_new\\CIKM2017_train\\train.txt','r')
+    f = open("E:\\Users\kingdom\TIANCHI\data\\train.txt", 'r')
     for line in f.readlines():
-        if(i >= k):
+        if (i >= k):
             line = line.split()
             labelSet = line[0].split(",")
             testName = labelSet[0]
@@ -23,28 +24,28 @@ def getDataSet1(n):
             line[0] = labelSet[2]
             dataSet = list(map(int, line))
             rainfall = float(rainfall)
-            if(rainfall <= 5 and rainfall>0 ):
+            if (rainfall <= 5 and rainfall >= 0):
                 testName_all.append(testName)
                 rainfall_all.append(rainfall)
                 dataSet_all.append(dataSet)
                 dataSet_label.append(1)
-            if(rainfall>5 and rainfall<=19 ):
+            if (rainfall > 5 and rainfall <= 19):
                 testName_all.append(testName)
                 rainfall_all.append(rainfall)
                 dataSet_all.append(dataSet)
                 dataSet_label.append(2)
-            if(rainfall>19 and rainfall<=42 ):
+            if (rainfall > 19 and rainfall <= 42):
                 testName_all.append(testName)
                 rainfall_all.append(rainfall)
                 dataSet_all.append(dataSet)
                 dataSet_label.append(3)
-            if(rainfall>42 ):
+            if (rainfall > 42):
                 testName_all.append(testName)
                 rainfall_all.append(rainfall)
                 dataSet_all.append(dataSet)
                 dataSet_label.append(4)
             print(i)
-            if(i == k+ 999):
+            if (i == k + 999):
                 break
         i += 1
     return testName_all, rainfall_all, dataSet_all, dataSet_label
@@ -57,9 +58,9 @@ def getDataSet2(n, label_num):
     dataSet_label = []
     i = 0
     k = 1000 * n
-    f = open('E:\\李卓聪\\rain_predict\\data_new\\CIKM2017_train\\train.txt','r')
+    f = open('E:\\Users\kingdom\TIANCHI\data\\train.txt', 'r')
     for line in f.readlines():
-        if(i >= k):
+        if (i >= k):
             line = line.split()
             labelSet = line[0].split(",")
             testName = labelSet[0]
@@ -67,28 +68,55 @@ def getDataSet2(n, label_num):
             line[0] = labelSet[2]
             dataSet = list(map(int, line))
             rainfall = float(rainfall)
-            if(rainfall <= 5 and rainfall>0 and label_num==0):
+            if (rainfall <= 5 and rainfall >= 0 and label_num == 0):
                 testName_all.append(testName)
                 rainfall_all.append(rainfall)
                 dataSet_all.append(dataSet)
                 dataSet_label.append(1)
-            if(rainfall>5 and rainfall<=19 and label_num==1):
+            if (rainfall > 5 and rainfall <= 19 and label_num == 1):
                 testName_all.append(testName)
                 rainfall_all.append(rainfall)
                 dataSet_all.append(dataSet)
                 dataSet_label.append(2)
-            if(rainfall>19 and rainfall<=42 and label_num==2):
+            if (rainfall > 19 and rainfall <= 42 and label_num == 2):
                 testName_all.append(testName)
                 rainfall_all.append(rainfall)
                 dataSet_all.append(dataSet)
                 dataSet_label.append(3)
-            if(rainfall>42 and label_num==3):
+            if (rainfall > 42 and label_num == 3):
                 testName_all.append(testName)
                 rainfall_all.append(rainfall)
                 dataSet_all.append(dataSet)
                 dataSet_label.append(4)
             print(i)
-            if(i == k+ 999):
+            if (i == k + 999):
+                break
+        i += 1
+    return testName_all, rainfall_all, dataSet_all, dataSet_label
+
+
+def getDataSet3(n):
+    testName_all = []
+    rainfall_all = []
+    dataSet_all = []
+    dataSet_label = []
+    i = 0
+    k = 1000 * n
+    f = open('E:\\Users\kingdom\TIANCHI\data\\testA.txt', 'r')
+    for line in f.readlines():
+        if (i >= k):
+            line = line.split()
+            labelSet = line[0].split(",")
+            testName = labelSet[0]
+            rainfall = labelSet[1]
+            line[0] = labelSet[2]
+            dataSet = list(map(int, line))
+            rainfall = float(rainfall)
+            testName_all.append(testName)
+            rainfall_all.append(rainfall)
+            dataSet_all.append(dataSet)
+            print(i)
+            if (i == k + 999):
                 break
         i += 1
     return testName_all, rainfall_all, dataSet_all, dataSet_label
@@ -97,17 +125,20 @@ def getDataSet2(n, label_num):
 def pre_train():
     dataSet_all = []
     rainfall_all = []
-    dataSet_label_all= []
+    dataSet_label_all = []
     print("aaaaaaaaaaaaaaaaa")
-    os.chdir("E:\\李卓聪\\save_file\\machine_model")
+    os.chdir("E:\\Users\kingdom\TIANCHI\data\\machine_model")
     ipca = joblib.load("rain_ipca_model.m")
     print("bbbbbbbbbbbbbbbb")
     for i in range(10):
         testName, rainfall, dataSet, dataSet_label = getDataSet1(i)
-        dataSet = ipca.fit_transform(dataSet)
+        dataSet = ipca.transform(dataSet)
         dataSet_all.extend(dataSet)
         rainfall_all.extend(rainfall)
         dataSet_label_all.extend(dataSet_label)
+
+    print(len(dataSet_all))
+    print(len(dataSet_label_all))
 
     gbc = GradientBoostingClassifier()
     gbc.fit(dataSet_all, dataSet_label_all)
@@ -116,48 +147,59 @@ def pre_train():
 
 
 def train():
-    dataSet_all = [[],[],[],[],[]]
-    rainfall_all = [[],[],[],[],[]]
-    dataSet_label_all = [[],[],[],[],[]]
+    dataSet_all = [[], [], [], [], []]
+    rainfall_all = [[], [], [], [], []]
+    dataSet_label_all = [[], [], [], [], []]
     print("aaaaaaaaaaaaaaaaa")
-    os.chdir("E:\\李卓聪\\save_file\\machine_model")
+    os.chdir("E:\\Users\kingdom\TIANCHI\data\\machine_model")
     ipca = joblib.load("rain_ipca_model.m")
     print("bbbbbbbbbbbbbbbb")
     for k in range(4):
         for i in range(10):
             testName, rainfall, dataSet, dataSet_label = getDataSet2(i, k)
-            dataSet = ipca.fit_transform(dataSet)
+            dataSet = ipca.transform(dataSet)
             dataSet_all[k].extend(dataSet)
             rainfall_all[k].extend(rainfall)
-            #dataSet_label_all[k].extend(dataSet_label) #此处不需要
-    print("cccccccccc")
-    for i in range(4):
+            # dataSet_label_all[k].extend(dataSet_label) #此处不需要
+            print("cccccccccc")
+
+        print(len((dataSet_all[k])))
+        print(len(rainfall_all[k]))
+        print(dataSet_all[k])
+        print(rainfall_all[k])
+
+        x = dataSet_all[k]
+        x = np.array(x)
+        print(type(x))
+        y = rainfall_all[k]
+        y = np.array(y)
         gbdt = ensemble.GradientBoostingRegressor()
-        gbdt.fit(dataSet_all[i], rainfall_all[i])  # 训练数据来学习，不需要返回值
-        os.chdir("E:\\李卓聪\\save_file\\machine_model")
-        joblib.dump(gbdt, "rain_train_GBDT_"+str(i)+".m")
+        gbdt.fit(x, y)  # 训练数据来学习，不需要返回值
+        os.chdir("E:\\Users\kingdom\TIANCHI\data\\machine_model")
+        joblib.dump(gbdt, "rain_train_GBDT_" + str(k) + ".m")
+        print("done = ", k)
         print("ddddddddddddddd")
 
 
 def predict():
-    os.chdir("E:\\李卓聪\\save_file\\machine_model")
+    os.chdir("E:\\Users\kingdom\TIANCHI\data\\machine_model")
     ipca = joblib.load("rain_ipca_model.m")
     gbc = joblib.load("rain_pretrain_GBDR.m")
-    gbdt1 = joblib.load("rain_train_GBDT_1.m")
-    gbdt2 = joblib.load("rain_train_GBDT_2.m")
-    gbdt3 = joblib.load("rain_train_GBDT_3.m")
-    gbdt4 = joblib.load("rain_train_GBDT_4.m")
+    gbdt1 = joblib.load("rain_train_GBDT_0.m")
+    gbdt2 = joblib.load("rain_train_GBDT_1.m")
+    gbdt3 = joblib.load("rain_train_GBDT_2.m")
+    gbdt4 = joblib.load("rain_train_GBDT_3.m")
     result = []
     for i in range(2):
         print("1")
-        testName, rainfall, dataSet, dataSet_label = getDataSet1(i)
+        testName, rainfall, dataSet, dataSet_label = getDataSet3(i)
         print("2")
-        dataSet = ipca.fit_transform(dataSet)
+        dataSet = ipca.transform(dataSet)
         print("2.5")
         predict_label = gbc.predict(dataSet)
         print("3")
         for k in range(1000):
-            if(predict_label[k] == 1):
+            if (predict_label[k] == 1):
                 y = gbdt1.predict(dataSet[k])
             if (predict_label[k] == 2):
                 y = gbdt2.predict(dataSet[k])
@@ -168,22 +210,19 @@ def predict():
             result.append(y)
 
     print("4")
-    save_file = "E:\\李卓聪\\save_file\\rainfall_predict\\" + "GBDT_result_1"  + ".txt"
+    save_file = "E:\\Users\kingdom\TIANCHI\data\\" + "GBDT_result_1" + ".txt"
     fl = open(save_file, 'w')
     for k in result:
-        fl.write(str(k))
+        fl.write(str(k[0]))
         fl.write("\n")
     fl.close()
     print("5")
 
 
-
-
-
 if __name__ == "__main__":
-    pre_train()
+    #pre_train()
     print("pretrain done!")
-    train()
+    #train()
     print("train done!")
     predict()
     print("finish!")
